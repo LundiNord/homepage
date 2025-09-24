6b01897d4b5b7cd6763fe6636ebf6aec87938428
Leon Bruns <ujiqk@student.kit.edu>
2025-09-24 12:06:01 +0200

    feat: groundwork for altitude charts

---
+++ hiking/nordkalottleden2025.js
@@ -0,0 +1,104 @@
+import { getStandardLeafletMap } from './hiking.minified.js';
+
+function getGPXLeafletMap(gpxTrack, containerID, infoDivID) {
+    const mapL = getStandardLeafletMap(containerID);
+    const options = {
+        async: true,
+        polyline_options: { color: 'red' },
+    };
+
+    var elevation_options = {
+        // Default chart colors: theme lime-theme, magenta-theme, ...
+        theme: "lightblue-theme",
+        // Chart container outside/inside map container
+        detached: true,
+        // if (detached), the elevation chart container
+        elevationDiv: infoDivID,
+        // if (!detached) autohide chart profile on chart mouseleave
+        autohide: false,
+        // if (!detached) initial state of chart profile control
+        collapsed: false,
+        // if (!detached) control position on one of the map corners
+        position: "topright",
+        // Toggle close icon visibility
+        closeBtn: false,
+        // Autoupdate map center on chart mouseover.
+        followMarker: true,
+        // Autoupdate map bounds on chart update.
+        autofitBounds: true,
+        // Chart distance/elevation units.
+        imperial: false,
+        // [Lat, Long] vs [Long, Lat] points. (leaflet default: [Lat, Long])
+        reverseCoords: false,
+        // Acceleration chart profile: true || "summary" || "disabled" || false
+        acceleration: false,
+        // Slope chart profile: true || "summary" || "disabled" || false
+        slope: false,
+        // Speed chart profile: true || "summary" || "disabled" || false
+        speed: "disabled",
+        // Altitude chart profile: true || "summary" || "disabled" || false
+        altitude: true,
+        // Display time info: true || "summary" || false
+        time: true,
+        // Display distance info: true || "summary" || false
+        distance: true,
+        // Summary track info style: "inline" || "multiline" || false
+        summary: 'multiline',
+        // Download link: "link" || false || "modal"
+        downloadLink: "link",
+        // Toggle chart ruler filter
+        ruler: true,
+        // Toggle chart legend filter
+        legend: true,
+        // Toggle "leaflet-almostover" integration
+        almostOver: false,
+        // Toggle "leaflet-distance-markers" integration
+        distanceMarkers: true,
+        // Toggle "leaflet-edgescale" integration
+        edgeScale: false,
+        // Toggle "leaflet-hotline" integration
+        hotline: true,
+        // Display track datetimes: true || false
+        timestamps: false,
+        // Display track waypoints: true || "markers" || "dots" || false
+        waypoints: true,
+        // Toggle custom waypoint icons: true || { associative array of <sym> tags } || false
+        wptIcons: {
+            '': L.divIcon({
+                className: 'elevation-waypoint-marker',
+                html: '<i class="elevation-waypoint-icon"></i>',
+                iconSize: [30, 30],
+                iconAnchor: [8, 30],
+            }),
+        },
+        // Toggle waypoint labels: true || "markers" || "dots" || false
+        wptLabels: true,
+        // Render chart profiles as Canvas or SVG Paths
+        preferCanvas: true,
+    };
+    // Instantiate elevation control.
+    var controlElevation = L.control.elevation(elevation_options).addTo(mapL);
+    controlElevation.load(gpxTrack);
+    return mapL;
+}
+
+
+
+document.addEventListener("DOMContentLoaded", () =>{
+    // const nordkalottleden2025Overview = getGPXLeafletMap("gpx/Nordkalottleden2025/Nordkalottleden2025_gesamt.gpx", 20, "nordkalottleden2025Overview", "#info_01");
+    const nordkalottleden2025Day1 = getGPXLeafletMap("gpx/Nordkalottleden2025/Nordkalottleden01_processed.gpx", "nordkalottleden2025_1_map", "#info_01");
+    // const nordkalottleden2025Day2 = getGPXLeafletMap("gpx/Nordkalottleden2025/Nordkalottleden02_processed.gpx", 2, "nordkalottleden2025_2_map");
+    // const nordkalottleden2025Day3 = getGPXLeafletMap("gpx/Nordkalottleden2025/Nordkalottleden03_processed.gpx", 3, "nordkalottleden2025_3_map");
+    // const nordkalottleden2025Day4 = getGPXLeafletMap("gpx/Nordkalottleden2025/Nordkalottleden04_processed.gpx", 4, "nordkalottleden2025_4_map");
+    // const nordkalottleden2025Day5 = getGPXLeafletMap("gpx/Nordkalottleden2025/Nordkalottleden05_processed.gpx", 5, "nordkalottleden2025_5_map");
+    // const nordkalottleden2025Day6 = getGPXLeafletMap("gpx/Nordkalottleden2025/Nordkalottleden06_processed.gpx", 6, "nordkalottleden2025_6_map");
+    // const nordkalottleden2025Day7 = getGPXLeafletMap("gpx/Nordkalottleden2025/Nordkalottleden07_processed.gpx", 7, "nordkalottleden2025_7_map");
+    // const nordkalottleden2025Day8 = getGPXLeafletMap("gpx/Nordkalottleden2025/Nordkalottleden08_processed.gpx", 8, "nordkalottleden2025_8_map");
+    // const nordkalottleden2025Day9 = getGPXLeafletMap("gpx/Nordkalottleden2025/Nordkalottleden09_processed.gpx", 9, "nordkalottleden2025_9_map");
+    // const nordkalottleden2025Day10 = getGPXLeafletMap("gpx/Nordkalottleden2025/Nordkalottleden10_processed.gpx", 10, "nordkalottleden2025_10_map");
+    // const nordkalottleden2025Day11 = getGPXLeafletMap("gpx/Nordkalottleden2025/Nordkalottleden11_processed.gpx", 11, "nordkalottleden2025_11_map");
+    // const nordkalottleden2025Day12 = getGPXLeafletMap("gpx/Nordkalottleden2025/Nordkalottleden12_processed.gpx", 12, "nordkalottleden2025_12_map");
+    // const nordkalottleden2025Day13 = getGPXLeafletMap("gpx/Nordkalottleden2025/Nordkalottleden13_processed.gpx", 13, "nordkalottleden2025_13_map");
+    // const nordkalottleden2025Day14 = getGPXLeafletMap("gpx/Nordkalottleden2025/Nordkalottleden14_processed.gpx", 14, "nordkalottleden2025_14_map");
+    // const nordkalottleden2025Day15 = getGPXLeafletMap("gpx/Nordkalottleden2025/Nordkalottleden15_processed.gpx", 15, "nordkalottleden2025_15_map");
+});
